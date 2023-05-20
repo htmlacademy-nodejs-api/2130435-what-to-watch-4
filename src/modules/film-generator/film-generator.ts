220cde6a320cc7b5961d6452cec0b88e0d14d4d0
Andy Rosa <andy.roshitu@gmail.com>
2023-05-20 15:51:30 +0300

    add: type for mock data server, film-generator

---
+++ src/modules/film-generator/film-generator.ts
@@ -0,0 +1,50 @@
+import {FilmGeneratorInterface} from './film-generator.interface';
+import type {MockData} from '../../types/mock-data.type';
+import {generateRandomValue, getRandomItem, getRandomItems} from '../../core/helpers';
+import {Genre} from '../../types/film.type';
+import dayjs from 'dayjs';
+
+enum MathDayWeek {
+  Min = 1,
+  Max = 7
+}
+
+export default class FilmGenerator implements FilmGeneratorInterface{
+  constructor(private readonly MockData: MockData) {}
+
+  public generate(): string {
+    const title = getRandomItem<string>(this.MockData.titles);
+    const description = getRandomItem<string>(this.MockData.descriptions);
+    const publicationDate = dayjs()
+      .subtract(
+        generateRandomValue(MathDayWeek.Min, MathDayWeek.Max), 'day'
+      ).toISOString();
+
+    const genre = getRandomItems<Genre>([
+      Genre.Comedy, Genre.Crime, Genre.Documentary, Genre.Drama,
+      Genre.Horror, Genre.Family, Genre.Romance, Genre.SciFi, Genre.Thriller]
+    ).join(', ');
+
+    const realiseDate = getRandomItem<string>(this.MockData.realiseDates);
+    const rating = getRandomItem<number>(this.MockData.rating);
+    const previewVideo = getRandomItem<string>(this.MockData.previewsVideo);
+    const videoLink = getRandomItem<string>(this.MockData.videoLinks);
+    const actors = getRandomItem<string[]>(this.MockData.actors).join(', ');
+    const director = getRandomItem<string>(this.MockData.directors);
+    const duration = getRandomItem<number>(this.MockData.durations);
+    const userName = getRandomItem<string>(this.MockData.userNames);
+    const userAvatar = getRandomItem<string>(this.MockData.userAvatars);
+    const userEmail = getRandomItem<string>(this.MockData.userEmails);
+    const userPassword = getRandomItem<string>(this.MockData.userPasswords);
+    const poster = getRandomItem<string>(this.MockData.posters);
+    const backgroundImage = getRandomItem<string>(this.MockData.backgroundImages);
+    const backgroundColor = getRandomItem<string>(this.MockData.backgroundColors);
+    return [
+      title, description, publicationDate,
+      genre, realiseDate, rating, previewVideo,
+      videoLink, actors, director, duration,
+      userName, userAvatar, userEmail, userPassword,
+      poster, backgroundImage, backgroundColor
+    ].join('\t');
+  }
+}
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,19 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  realiseDates: string[];
+  rating: number[];
+  previewsVideo: string[];
+  videoLinks: string[];
+  actors: Array<string[]>;
+  directors: string[];
+  durations: number[];
+  commentsCount?: number;
+  userNames: string[];
+  userAvatars: string[];
+  userEmails: string[];
+  userPasswords: string[];
+  posters: string[];
+  backgroundImages: string[];
+  backgroundColors: string[];
+}
