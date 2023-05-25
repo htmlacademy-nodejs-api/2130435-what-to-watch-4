6613aca7d834c5712493f002842db0e996dbbb63
Andy Rosa <andy.roshitu@gmail.com>
2023-05-26 02:46:41 +0300

    add: template film entity. refactor: rename AppComponent

--- src/types/app-components.enum.ts
+++ src/types/app-components.enum.ts
@@ -1,5 +1,8 @@
-export const AppComponents = {
+export const AppComponent = {
   RestApplication: Symbol.for('RestApplication'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseClientInterface: Symbol.for('DatabaseClientInterface'),
+  UserServiceInterface: Symbol.for('UserServiceInterface'),
+  UserModel: Symbol.for('UserModel'),
 } as const;
