ce3993c4a3838ee04967c76b751919115fa60d8f
Andy Rosa <andy.roshitu@gmail.com>
2023-05-26 02:38:52 +0300

    upgrade: rest, decomposition: main container

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 import RestApplication from './app/rest.js';
-import {AppComponents} from './types/app-components.enum.js';
-import {LoggerInterface} from './core/logger/logger.interface.js';
-import {ConfigInterface} from './core/config/config.interface.js';
-import {RestSchema} from './core/config/rest.schema.js';
-import PinoService from './core/logger/pino.service.js';
-import ConfigService from './core/config/config.service.js';
+import {AppComponent} from './types/app-components.enum.js';
+import {createRestApplicationContainer} from './app/rest.container.js';
+import { createUserContainer } from './modules/user/user.container.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(AppComponents.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<LoggerInterface>(AppComponents.LoggerInterface).to(PinoService).inSingletonScope();
-  container.bind<ConfigInterface<RestSchema>>(AppComponents.ConfigInterface).to(ConfigService).inSingletonScope();
+  const mainContainer = Container.merge(
+    createRestApplicationContainer(),
+    createUserContainer(),
+  );
 
-  const application = container.get<RestApplication>(AppComponents.RestApplication);
+  const application = mainContainer.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
 }
 
