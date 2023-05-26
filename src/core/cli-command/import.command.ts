6bc37263b3b6e4054ea6422560b9dc5e07badef3
Andy Rosa <andy.roshitu@gmail.com>
2023-05-26 10:39:34 +0300

    WIP. upgrade import command

--- src/core/cli-command/import.command.ts
+++ src/core/cli-command/import.command.ts
@@ -2,9 +2,37 @@ import {CliCommandInterface} from './cli-command.interface';
 import {createFilm, getErrorMessage} from '../helpers/index.js';
 import TSVFileReader from '../file-reader/tsv-file-reader.js';
 import {CommandName} from './command-name.enum.js';
+import { LoggerInterface } from '../logger/logger.interface.js';
+import { DatabaseClientInterface } from '../database-client/database-client.interface.js';
+import { FilmServiceInterface } from '../../modules/film/film-service.interface.js';
+import { UserServiceInterface } from '../../modules/user/user-service.interface.js';
+import ConsoleLoggerService from '../logger/console.service.js';
+import FilmService from '../../modules/film/film.service.js';
+import { FilmModel } from '../../modules/film/film.entity.js';
+import { UserModel} from '../../modules/user/user.entity.js';
+import UserService from '../../modules/user/user.service.js';
+import MongoClientService from '../database-client/mongo-client.service.js';
+
+const DEFAULT_DB_PORT = '27017';
+const DEFAULT_USER_PASSWORD = '123456';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name: CommandName = CommandName.Import;
+  private userService!: UserServiceInterface;
+  private filmService!: FilmServiceInterface;
+  private databaseService!: DatabaseClientInterface;
+  private logger: LoggerInterface;
+  private salt!: string;
+
+  constructor() {
+    this.onLine = this.onLine.bind(this);
+    this.onComplete = this.onComplete.bind(this);
+
+    this.logger = new ConsoleLoggerService();
+    this.filmService = new FilmService(this.logger, FilmModel);
+    this.userService = new UserService(this.logger, UserModel);
+    this.databaseService = new MongoClientService(this.logger);
+  }
 
   private onLine(line: string) {
     const offer = createFilm(line);
